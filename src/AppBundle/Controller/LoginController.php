cd47e7f6ba41f203e84f4df7792b560aaa460be3
Кудаков Андрей <kudakovandrey@gmail.com>
2018-04-10 13:46:12 +0300

    task #49416 Добавлен контроллер входа на сайт

---
+++ src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+        $errors = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('library/forms/login.html.twig', array(
+            'errors' => $errors,
+            'username' => $lastUsername
+        ));
+    }
+}
