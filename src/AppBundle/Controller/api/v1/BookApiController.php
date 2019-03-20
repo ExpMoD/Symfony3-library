95007e1bf5fd09552da21b1726fb9e32b33aeaa8
ExpMoD <kudakovandrey@gmail.com>
2019-03-20 16:49:09 +0300

    Api v1. Book add

--- src/AppBundle/Controller/api/v1/BookApiController.php
+++ src/AppBundle/Controller/api/v1/BookApiController.php
@@ -71,4 +71,47 @@ class BookApiController extends Controller
 
         return $response->getResponse();
     }
+
+    /**
+     * @Route("/book/add")
+     */
+    public function bookAddAction(Request $request, ResponseApiHandler $response, TagAwareAdapter $cache)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        if (!$this->checkApiKey($request->get('api-key'))) {
+            $response->setMessage('Invalid api key', Response::HTTP_BAD_REQUEST);
+        } else {
+            $sName = $request->get('name');
+            $sAuthor = $request->get('author');
+
+            if (empty($sName)) {
+                $response->setMessage('Missed required field "name"', Response::HTTP_BAD_REQUEST);
+            }
+
+            if (empty($sAuthor)) {
+                $response->setMessage('Missed required field "author"', Response::HTTP_BAD_REQUEST);
+            }
+
+            if (!$response->existError()) {
+                $bookEntity = new Book();
+
+                $bookEntity->setName($sName);
+                $bookEntity->setAuthor($sAuthor);
+
+                if (!empty($request->get('dateOfReading'))) {
+                    $bookEntity->setDateOfReading(new \DateTime($request->get('dateOfReading')));
+                }
+
+                $manager->persist($bookEntity);
+                $manager->flush();
+
+                $cache->invalidateTags([$this->getParameter('book_list_cache_key')]);
+
+                $response->setStatusCode(Response::HTTP_OK);
+            }
+        }
+
+        return $response->getResponse();
+    }
 }
